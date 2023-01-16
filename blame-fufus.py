79d98e03e1bf03d6cf83359831a99595767f1d50
Matthew Oshimo <moshimo90696@gmail.com>
2023-01-16 14:21:49 -0800

    got repo to go to right directory

--- blame-fufus.py
+++ blame-fufus.py
@@ -5,7 +5,16 @@ from pathlib import Path
 # For all you know, the first argument to Repo is a path to the repository
 # # you want to work with
 # tester = Path().resolve()
-repo = Repo.init("")
-name = repo.active_branch
-# print(tester)
-print(name)
+
+def getInformation(directory):
+    repo = Repo(directory)
+    name = repo.active_branch
+    user = repo.iter_commits()
+    print(repo.is_dirty())  # check the dirty state)
+    print(user)
+    for t in user:
+        print(t.committed_date)
+    print(name)
+
+direct = "/Users/moshi/projects/project-gamma"
+getInformation(direct)
