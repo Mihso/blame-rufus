5e2b9f9ac7e7edbb02d1a09f362de139abb95b55
Matthew Oshimo <moshimo90696@gmail.com>
2023-01-16 16:07:22 -0800

    initial solution where everything works

--- blame-fufus.py
+++ blame-fufus.py
@@ -1,5 +1,6 @@
 from git import Repo
 from pathlib import Path
+import time
 
 # rorepo is a Repo instance pointing to the git-python repository.
 # For all you know, the first argument to Repo is a path to the repository
@@ -10,11 +11,24 @@ def getInformation(directory):
     repo = Repo(directory)
     name = repo.active_branch
     user = repo.iter_commits()
-    print(repo.is_dirty())  # check the dirty state)
-    print(user)
+    recentChange = False
+    rufus_fault = False
+    checked = False
     for t in user:
-        print(t.committed_date)
-    print(name)
+        count += 1
+        current = time.gmtime()
+        dateCommit = time.gmtime(t.committed_date)
+        if checked == False:
+            if current.tm_mday - dateCommit.tm_mday <= 7: # checks if most recent change happened within a week(7 days)
+                recentChange = True # returns true if so
+                if str(t.author).lower() == "rufus": # then checks if that change was made by rufus
+                    rufus_fault = True # returns true if so
+            checked = True
+
+    print("Active Branch: ", name)
+    print("Local Changes:" , repo.is_dirty())
+    print("Recent Commits: ",recentChange)
+    print("Blame Rufus: ",rufus_fault)
 
 direct = "/Users/moshi/projects/project-gamma"
 getInformation(direct)
